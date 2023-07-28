4d6d3056978f2a64e57840c1b21bc4f6491fd4a9
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-07-28 16:56:31 -0300

    Criacao do projeto rh-payroll

---
+++ rh-payroll/src/test/java/com/bruno/rhpayroll/RhPayrollApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.rhpayroll;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhPayrollApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
