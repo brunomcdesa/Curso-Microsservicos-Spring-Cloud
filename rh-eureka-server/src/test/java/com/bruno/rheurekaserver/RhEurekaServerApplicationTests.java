938d6d383b6622eab10665cc9fb9cff6ad7d4194
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-09 16:31:43 -0300

    Projeto rh-eureka-server criado

---
+++ rh-eureka-server/src/test/java/com/bruno/rheurekaserver/RhEurekaServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.bruno.rheurekaserver;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class RhEurekaServerApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
