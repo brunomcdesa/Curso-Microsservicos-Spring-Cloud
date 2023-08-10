c19bc968b055cf3069b8d9ef7cebdb74d5744e9a
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-10 17:22:43 -0300

    Configura Eureka Client

--- rh-payroll/src/main/java/com/bruno/rhpayroll/feignclients/WorkerClient.java
+++ rh-payroll/src/main/java/com/bruno/rhpayroll/feignclients/WorkerClient.java
@@ -9,7 +9,6 @@ import org.springframework.web.bind.annotation.PathVariable;
 
 @Component
 @FeignClient(name = "rh-worker",
-            url = "localhost:8001",
             path = "/workers")
 public interface WorkerClient {
 
--- rh-worker/src/main/java/com/bruno/rhworker/RhWorkerApplication.java
+++ rh-worker/src/main/java/com/bruno/rhworker/RhWorkerApplication.java
@@ -2,8 +2,10 @@ package com.bruno.rhworker;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RhWorkerApplication {
 
 	public static void main(String[] args) {
