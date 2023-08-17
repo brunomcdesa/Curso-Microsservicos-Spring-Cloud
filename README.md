00ce2200ed6a24bb29ec13c7026144ec05f5d19a
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-17 16:52:31 -0300

    rh-worker como um config server client

--- README.md
+++
@@ -1,1 +0,0 @@
-# Curso-Microsservicos-Spring-Cloud
--- rh-worker/src/main/java/com/bruno/rhworker/controller/WorkerController.java
+++ rh-worker/src/main/java/com/bruno/rhworker/controller/WorkerController.java
@@ -5,6 +5,7 @@ import com.bruno.rhworker.repositories.WorkerRepository;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.core.env.Environment;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
@@ -23,6 +24,16 @@ public class WorkerController {
 
     private final WorkerRepository workerRepository;
     private final Environment env;
+
+    @Value("${test.config}")
+    private String testConfig;
+
+    @GetMapping("configs")
+    public ResponseEntity<Void> getConfigs() {
+        log.info("Config = " + testConfig);
+        return ResponseEntity.noContent().build();
+    }
+
     @GetMapping
 	public ResponseEntity<List<Worker>> findAll() {
         var lista = workerRepository.findAll();
