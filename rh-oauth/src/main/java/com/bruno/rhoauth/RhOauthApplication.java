17ca16d1931c4bb59cf785186fdd0cda0a069efd
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-21 14:05:14 -0300

    Configurando projeto rh-oauth

--- rh-oauth/src/main/java/com/bruno/rhoauth/RhOauthApplication.java
+++ rh-oauth/src/main/java/com/bruno/rhoauth/RhOauthApplication.java
@@ -2,8 +2,10 @@ package com.bruno.rhoauth;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class RhOauthApplication {
 
     public static void main(String[] args) {
