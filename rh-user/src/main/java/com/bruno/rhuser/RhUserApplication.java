88f7e6577b605f53b509d8217f5998a2e1488d7b
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-21 11:55:24 -0300

    Database seeding

--- rh-user/src/main/java/com/bruno/rhuser/RhUserApplication.java
+++ rh-user/src/main/java/com/bruno/rhuser/RhUserApplication.java
@@ -1,15 +1,25 @@
 package com.bruno.rhuser;
 
+import lombok.RequiredArgsConstructor;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
 
 @SpringBootApplication
 @EnableEurekaClient
-public class RhUserApplication {
+@RequiredArgsConstructor
+public class RhUserApplication implements CommandLineRunner {
+
+    private final BCryptPasswordEncoder passwordEncoder;
 
     public static void main(String[] args) {
         SpringApplication.run(RhUserApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) throws Exception {
+       // System.out.println("BCRYPT = " + passwordEncoder.encode("123456"));
+    }
 }
