e99cff327a0fa7fd9c320c7063df1cf943f1306d
Bruno Cardoso <bruno.cardoso@xbrain.com.br>
2023-08-21 15:56:37 -0300

    Login, JWT token

--- rh-oauth/src/main/java/com/bruno/rhoauth/services/UserService.java
+++ rh-oauth/src/main/java/com/bruno/rhoauth/services/UserService.java
@@ -4,12 +4,15 @@ import com.bruno.rhoauth.entities.User;
 import com.bruno.rhoauth.feignclients.UserClient;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
 import org.springframework.stereotype.Service;
 
 @Service
 @RequiredArgsConstructor
 @Slf4j
-public class UserService {
+public class UserService implements UserDetailsService {
 
     private final UserClient userClient;
 
@@ -20,6 +23,17 @@ public class UserService {
             throw new IllegalArgumentException("Email não encontrado.");
         }
         log.info("Email encontrado: {}", email);
+        return user;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) {
+        var user = userClient.findByEmail(username).getBody();
+        if (user == null) {
+            log.error("Email não encontrado: {}", username);
+            throw new UsernameNotFoundException("Email não encontrado.");
+        }
+        log.info("Email encontrado: {}", username);
         return user;
     }
 }
